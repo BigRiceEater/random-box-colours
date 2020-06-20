8a9869f255d7d737297e9b29d14f15b12b9ecb4b
BigRiceEater <34292181+BigRiceEater@users.noreply.github.com>
2020-06-20 12:47:15 +0800

    Load jquery dependency with requirejs

--- script/main.js
+++ script/main.js
@@ -1,3 +1,11 @@
-requirejs(['controller'], function(controller){
+requirejs.config({
+    baseUrl: 'script',
+    paths: {
+        jquery: 'https://code.jquery.com/jquery-3.5.1.min',
+        jqueryui : "https://code.jquery.com/ui/1.12.1/jquery-ui.min"
+    }
+});
 
+requirejs(['controller'], function(controller){
+    console.log('main started')
 })
